3465a4654c00274df814ee1dbb32a602077eae3b
Carlos Padron <c_0509@hotmail.com>
2021-03-28 21:18:12 -0600

    product model created

---
+++ Model/ProductModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+let prodcutSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "El nombre del producto es requerido"],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "EL precio del producto es requerido"],
+    trim: true,
+  },
+  quantity: {
+    type: Number,
+    required: [true, "La cantidad total del producto es requerida"],
+    trim: true,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  image: {
+    type: Buffer,
+  },
+  articleType: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "articleType",
+  },
+  brand: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brand",
+  },
+  history: [
+    {
+      date: {
+        type: Date,
+      },
+      quantity: {
+        type: Number,
+        require: [true, "La cantidad ingresada es requerida"],
+      },
+      madeBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+});
+
+let productModel = new mongoose.model("Product", productModel);
+
+module.exports = productModel;
--- Public/js/productosClient.js
+++ Public/js/productosClient.js
@@ -1,1 +1,367 @@
-console.log('productosClient');
+window.addEventListener("DOMContentLoaded", () => {
+  //Variables & Elements
+  let $fields = [
+    "_id",
+    "name",
+    "image",
+    "price",
+    "quantity",
+    "type",
+    "articleType",
+  ];
+  let routes = {
+    get: "/getProducts",
+    add: "/addProduct",
+    update: "/updateProduct",
+    delete: "/deleteProduct",
+  };
+
+  let productsData = [];
+
+  const searchBtn = document.querySelector("#btnSearch");
+  const searchForm = document.querySelector("#searchForm");
+  const btnClearSearch = document.querySelector("#btnClearSearch");
+
+  //Listeners
+  searchBtn.addEventListener("click", search);
+  btnClearSearch.addEventListener("click", clearSearch);
+
+  //functions
+  async function search() {
+    blockElem(searchForm);
+
+    let body = {};
+
+    $fields.forEach((elem) => {
+      let elemData = document.querySelector(`[data-search="${elem}"]`);
+
+      if (elemData != undefined) {
+        switch (elem) {
+          case "disabled":
+            body[elem] = elemData.checked;
+            break;
+
+          default:
+            let data = elemData.value.trim();
+            body[elem] = data;
+            break;
+        }
+      }
+    });
+
+    try {
+      body = JSON.stringify(body);
+
+      let request = await fetch(routes.get, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          credentials: "same-origin",
+        },
+        body,
+      });
+
+      let json = await request.json();
+      console.log(json);
+      if (json.error) {
+        if (Array.isArray(json.message)) {
+          let messages = "";
+          json.message.forEach((msg) => {
+            messages += `<strong>*${msg}</strong> <br>`;
+          });
+          modalAlert("warning", "Aviso", messages);
+          unblockElem(searchForm);
+          return;
+        } else {
+          modalAlert(
+            "warning",
+            "Aviso",
+            `<strong>*${json.message}</strong> <br>`
+          );
+          unblockElem(searchForm);
+          return;
+        }
+      }
+
+      brandsData = json.response;
+
+      brandsData.forEach((elem, index) => {
+        elem.actions = `<div class="btn-group">
+          <button title="Editar"   type="button" class="btn btn-sm btn-icon btn-info   show"   style="border-top-left-radius: 1rem; border-bottom-left-radius: 1rem;"  data-index="${index}" data-id="${elem._id}" > <i class="uil uil-pen show"></i> </button>
+          <button title="Deshabilitar" type="button" class="btn btn-sm btn-icon btn-danger delete" style="border-top-right-radius: 1rem; border-bottom-right-radius: 1rem;"  data-index="${index}" data-id="${elem._id}" > <i class="uil uil-multiply delete"></i> </button>
+      </div>`;
+      });
+
+      mainTable.reloadTable(brandsData);
+      unblockElem(searchForm);
+    } catch (error) {
+      unblockElem(searchForm);
+      warningNotification("Error interno del servidor");
+      console.error(error);
+    }
+  }
+
+  async function save(route) {
+    resetFormValidation();
+
+    let response = validateForm();
+
+    if (response.valid === false) {
+      return;
+    }
+
+    disableButton(addBrandBtn, "Agregando");
+
+    try {
+      let body = JSON.stringify(response.body);
+
+      let request = await fetch(route, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          credentials: "same-origin",
+        },
+        body,
+      });
+
+      let json = await request.json();
+
+      if (json.error) {
+        if (Array.isArray(json.message)) {
+          let messages = "";
+          json.message.forEach((msg) => {
+            messages += `<strong>*${msg}</strong> <br>`;
+          });
+          enableButton(addBrandBtn, "Agregar");
+
+          modalAlert("warning", "Aviso", messages);
+          return;
+        } else {
+          modalAlert(
+            "warning",
+            "Aviso",
+            `<strong>${json.message}</strong> <br>`
+          );
+          return;
+        }
+      }
+      enableButton(addBrandBtn, "Agregar");
+
+      modalAlert(
+        "success",
+        "Aviso ",
+        `<strong>${json.message}</strong> <br>`,
+        () => {
+          $("#main_modal").modal("hide");
+          search();
+        }
+      );
+    } catch (error) {
+      warningNotification("Error interno del servidor");
+      enableButton(addBrandBtn, "Agregar");
+      console.error(error);
+    }
+  }
+
+  async function destroy(_id) {
+    blockElem(mainTableBody);
+    let body = JSON.stringify({ _id });
+
+    try {
+      let request = await fetch(routes.delete, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          credentials: "same-origin",
+        },
+        body,
+      });
+
+      let json = await request.json();
+
+      if (json.error) {
+        if (Array.isArray(json.message)) {
+          let messages = "";
+          json.message.forEach((msg) => {
+            messages += `<strong>*${msg}</strong>`;
+          });
+          unblockElem(mainTableBody);
+          modalAlert("warning", "Aviso", messages);
+          return;
+        } else {
+          unblockElem(mainTableBody);
+
+          modalAlert(
+            "warning",
+            "Aviso",
+            `<strong>*${json.message}</strong> <br>`
+          );
+          return;
+        }
+      }
+
+      modalAlert(
+        "success",
+        "Aviso ",
+        `<strong>${json.message}</strong> <br>`,
+        () => {
+          search();
+        }
+      );
+    } catch (error) {
+      warningNotification(error);
+      unblockElem(mainTableBody);
+      console.error(error);
+    }
+  }
+
+  function validateForm() {
+    let body = {};
+    let valid = true;
+
+    $fields.forEach((elem) => {
+      let data;
+      let msg;
+      switch (elem) {
+        case "_id":
+          data = document.querySelector(`#${elem}`);
+          body[elem] = data.value;
+          break;
+
+        case "name":
+          data = document.querySelector(`#${elem}`);
+          msg = document.querySelector(`#${elem}Msg`);
+
+          if (data.value === "") {
+            data.classList.add("invalid-input");
+            msg.innerHTML += "El nombre es requerido.";
+            msg.classList.add("text-danger");
+            valid = false;
+          }
+          body[elem] = data.value;
+          break;
+        case "disabled":
+          data = document.querySelector(`#${elem}`);
+          body[elem] = data.checked;
+          break;
+        default:
+          break;
+      }
+    });
+
+    return {
+      valid,
+      body,
+    };
+  }
+
+  async function resetForm(form) {
+    switch (form) {
+      case "brandsForm":
+        document.querySelector("#brandsForm").reset();
+        addBrandBtn.classList.remove("d-none");
+        updateBrandBtn.classList.add("d-none");
+        break;
+      case "searchForm":
+        document.querySelector("#searchForm").reset();
+        break;
+      default:
+        break;
+    }
+  }
+
+  function resetFormValidation() {
+    $fields.forEach((elem) => {
+      let msg = document.querySelector(`#${elem}Msg`);
+      let field = document.querySelector(`#${elem}`);
+
+      if (field) {
+        field.classList.remove("invalid-input");
+      }
+      if (msg) {
+        msg.innerHTML = "";
+        msg.classList.remove("text-danger");
+      }
+    });
+  }
+
+  function deleteConfirmation(_id) {
+    confirmationAlert("Se deshabilitará la marca seleccionada", () => {
+      destroy(_id);
+    });
+  }
+
+  function clearSearch() {
+    resetForm("searchForm");
+  }
+
+  function rowClicked(e) {
+    if (e.target && e.target.classList.contains("show")) {
+      if (e.target.tagName === "I") {
+        let button = e.target.parentElement;
+        let index = button.getAttribute("data-index");
+        showMainModalEdit(brandsData[index]);
+      } else {
+        let button = e.target;
+        let index = button.getAttribute("data-index");
+        showMainModalEdit(brandsData[index]);
+      }
+    }
+
+    if (e.target && e.target.classList.contains("delete")) {
+      if (e.target.tagName === "I") {
+        let button = e.target.parentElement;
+        let index = button.getAttribute("data-index");
+        deleteConfirmation(brandsData[index]._id);
+      } else {
+        let button = e.target;
+        let index = button.getAttribute("data-index");
+        deleteConfirmation(brandsData[index]._id);
+      }
+    }
+  }
+
+  function showMainModalAdd() {
+    document.querySelector("#modal_title").innerHTML =
+      "Agregar una nueva marca";
+    $("#main_modal").modal("show");
+  }
+
+  function showMainModalEdit(brand) {
+    addBrandBtn.classList.add("d-none");
+    updateBrandBtn.classList.remove("d-none");
+
+    $fields.forEach((elem) => {
+      switch (elem) {
+        case "disabled":
+          document.querySelector(`#${elem}`).checked = brand[elem];
+          break;
+
+        default:
+          document.querySelector(`#${elem}`).value = brand[elem];
+          break;
+      }
+    });
+
+    document.querySelector("#modal_title").innerHTML = "Editar marca";
+    $("#main_modal").modal("show");
+  }
+
+  $("#main_modal").on("hidden.bs.modal", function (e) {
+    resetFormValidation();
+    resetForm("brandsForm");
+  });
+
+  //Initial actions
+  let mainTable = new CardTable(
+    "mainTable",
+    productsData,
+    "btnNext",
+    "btnPrev",
+    "pageCounter"
+  );
+  mainTable.reloadCardTable(productsData);
+});

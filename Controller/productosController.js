65de8e7d288d8f5f01990e1c8c5fb1a4b81cac83
Carlos Padron <c_0509@hotmail.com>
2021-04-05 15:23:44 -0500

    endpoint to update product. It's not tested yet

--- Controller/productosController.js
+++ Controller/productosController.js
@@ -265,4 +265,5 @@ module.exports = {
   index,
   createProduct,
   searchProducts,
+  updateProduct
 };
--- Routes/productosRoutes.js
+++ Routes/productosRoutes.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const router = new express.Router()
+const express = require("express");
+const router = new express.Router();
 
-const productosController = require('../Controller/productosController')
+const productosController = require("../Controller/productosController");
 const { authViews, authRoute } = require("../Utils/Middlewares/authMiddleware");
 
-router.get('/inventario/productos', authViews, productosController.index )
+router.get("/inventario/productos", authViews, productosController.index);
 
-router.post('/getProducts', authRoute, productosController.searchProducts )
+router.post("/getProducts", authRoute, productosController.searchProducts);
 
-router.post('/addProduct', authRoute, productosController.createProduct )
+router.post("/addProduct", authRoute, productosController.createProduct);
 
-//router.post('/updateProduct', authRoute, productosController.createProduct )
+router.post("/updateProduct", authRoute, productosController.updateProduct);
 
 //router.post('/deleteProduct', authRoute, productosController.createProduct )
 
-
-module.exports = router
+module.exports = router;

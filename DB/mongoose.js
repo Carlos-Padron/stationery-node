e441bb1acd0efc01e09cbb48c11fafb9010ff17b
Carlos Padron <c_0509@hotmail.com>
2021-05-22 01:01:41 -0500

    catch block in db file to log errors

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const connectionURL = process.env.DATABASE;
 
-const mongooseConnection = mongoose.connect(connectionURL, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+const mongooseConnection = mongoose
+  .connect(connectionURL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .catch((err) => {
+    console.error("error connection to the database:");
+    console.error(err);
+  });
 
 module.exports = mongooseConnection;
